5149546a1d59a2f819124c5cdfb21d4a795064a1
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-02-25 22:46:51 -0500

    Update theme_clickpoint.php

--- lang/es/theme_clickpoint.php
+++ lang/es/theme_clickpoint.php
@@ -1,166 +1,161 @@
 <?php
-$string['pluginname'] = 'clickpoint';
-$string['choosereadme'] = 'IngeWeb es un tema creado por el Ing. Pablo A Pico (<a href="http://ingeweb.co/">IngeWeb</a>) exclusivamente para nuestros clientes.';
+$string['pluginname'] = 'ClickPoint';
+$string['choosereadme'] = 'ClickPoint es un tema creado por Pedro Arias exclusivamente para nuestros clientes.';
 
-$string['configtitle'] = 'clickpoint';
+$string['configtitle'] = 'ClickPoint';
 
-$string['logo'] = 'Logo';
+$string['logo'] = 'Logotipo';
 
-$string['themesettings'] = 'Configuración del Tema clickpoint';
-$string['themesettingsgeneral'] = 'Configuración General';
+$string['themesettings'] = 'Ajustes del tema ClickPoint';
+$string['themesettingsgeneral'] = 'Ajustes generales';
 $string['contactinfo'] = 'Información de contacto';
 $string['abouttitle'] = 'Título Acerca de';
-$string['abouttitledesc'] = 'Título para la sección "Acerca de" en el pie de página';
-$string['abouttitle_default'] = 'Acerca de Nosotros';
+$string['abouttitledesc'] = 'Título para "Acerca de" en el área del pie de página';
+$string['abouttitle_default'] = 'Sobre Nosotros';
 
 $string['abouttext'] = 'Texto Acerca de';
-$string['abouttextdesc'] = 'Contenido para la sección "Acerca de" en el pie de página';
+$string['abouttextdesc'] = 'Contenido para "Acerca de" en el área del pie de página';
 
-$string['loginfootertext'] = 'Pie de página del login';
+$string['loginfootertext'] = 'Pie de página de inicio de sesión';
 $string['loginfootertextdesc'] = 'Contenido para el área del pie de página en la página de inicio de sesión';
 
 $string['generalalert'] = 'Alerta General';
-$string['generalalertdesc'] = 'Esto mostrará una alerta en su página principal para informar a los usuarios sobre un evento/situación particular.';
+$string['generalalertdesc'] = 'Esto mostrará una alerta en tu página principal para avisar a los usuarios de un evento o situación particular.';
 
-$string['footertitle'] = 'Título del Pie de Página Central';
+$string['footertitle'] = 'Título del pie de página central';
 $string['footertitledesc'] = 'El título para el módulo central en el área del pie de página.';
 
-$string['footertext'] = 'Texto del Pie de Página Central';
+$string['footertext'] = 'Texto del pie de página central';
 $string['footertextdesc'] = 'El contenido para el módulo central en el área del pie de página.';
 
-$string['footertitle2'] = 'Título del Pie de Página Derecho';
+$string['footertitle2'] = 'Título del pie de página derecho';
 $string['footertitledesc2'] = 'El título para el módulo derecho en el área del pie de página.';
 
-$string['footertext2'] = 'Texto del Pie de Página Derecho';
+$string['footertext2'] = 'Texto del pie de página derecho';
 $string['footertextdesc2'] = 'El contenido para el módulo derecho en el área del pie de página.';
 
-$string['copyright'] = 'Copyright';
+$string['copyright'] = 'Derechos de autor';
 $string['copyrightdesc'] = 'Acerca de este sitio web.';
 
-$string['customcss'] = 'CSS Personalizado';
-$string['customcssdesc'] = 'Cualquier regla CSS que agregue a esta área se reflejará en cada página, facilitando la personalización de este tema.';
+$string['customcss'] = 'CSS personalizado';
+$string['customcssdesc'] = 'Cualquier regla CSS que agregues en esta área se reflejará en cada página, facilitando la personalización de este tema.';
 
-$string['themeinfotext'] = 'Este tema fue creado para <strong>clickpoint</strong> por <a target="_blank" href="http://ingeweb.co/">IngeWeb - Soluciones para triunfar en Internet</a>.';
-$string['credit'] = 'Copyright © 2024 - Todos los derechos reservados';
+$string['themeinfotext'] = 'Este tema fue creado para <strong>ClickPoint</strong> por <a target="_blank" href="https://orioncloud.com.co/">OrionCloud Solutions</a>.';
+$string['credit'] = 'Copyright © 2025 - Todos los derechos reservados';
 
-$string['showingonlycategorieswithenrolledcourses'] = 'Mostrando solo categorías con cursos matriculados';
+$string['showingonlycategorieswithenrolledcourses'] = 'Mostrando solo categorías que contienen cursos inscritos';
 $string['nocoursestoshow'] = 'No se encontraron cursos para mostrar';
 
 $string['region-side-pre'] = 'Derecha';
 
-// Funcionalidad de Copiar/Pegar
+// Cadenas existentes para la funcionalidad de Copiar/Pegar
 $string['copypaste'] = 'Copiar y Pegar';
-$string['copypaste_desc'] = 'Habilitar o deshabilitar copiar y pegar para estudiantes';
+$string['copypaste_desc'] = 'Habilitar o deshabilitar copiar y pegar para los estudiantes';
 $string['config_copypaste'] = 'Habilitar o deshabilitar copiar y pegar';
-$string['config_copypaste_desc'] = 'Habilitar o deshabilitar copiar y pegar para estudiantes';
+$string['config_copypaste_desc'] = 'Habilitar o deshabilitar copiar y pegar para los estudiantes';
 $string['disable'] = 'Deshabilitar';
 $string['enable'] = 'Habilitar';
 
 $string['show'] = 'Mostrar';
 $string['hide'] = 'Ocultar';
 
-// Configuraciones del carrusel
-$string['carouselsettings'] = 'Configuración del Carrusel';
-$string['carouselsettings_desc'] = 'Configure los ajustes para el carrusel de la página de inicio de sesión incluyendo imágenes, textos y otras opciones.';
+// Cadenas existentes para el carrusel
+$string['carouselsettings'] = 'Ajustes del carrusel';
+$string['carouselsettings_desc'] = 'Configura los ajustes para el carrusel de la página de inicio de sesión, incluyendo imágenes, textos y otras opciones.';
 
-$string['enablecarousel'] = 'Habilitar carrusel de la página de inicio';
-$string['enablecarousel_desc'] = 'Habilite esta configuración para mostrar un carrusel de imágenes en la página de inicio de sesión.';
+$string['enablecarousel'] = 'Habilitar carrusel en la página de inicio de sesión';
+$string['enablecarousel_desc'] = 'Habilita esta opción para mostrar un carrusel de imágenes en la página de inicio de sesión.';
 $string['numberofslides'] = 'Número de diapositivas';
-$string['numberofslides_desc'] = 'Seleccione el número de diapositivas para mostrar en el carrusel.';
-$string['slideimage'] = 'Imagen de diapositiva {$a}';
-$string['slideimage_desc'] = 'Subir una imagen para la diapositiva {$a} en el carrusel.';
-$string['carouselinterval'] = 'Intervalo del Carrusel';
-$string['carouselintervaldesc'] = 'Ingrese el tiempo de intervalo en milisegundos para las diapositivas del carrusel.';
+$string['numberofslides_desc'] = 'Selecciona el número de diapositivas a mostrar en el carrusel de la página de inicio de sesión.';
+$string['slideimage'] = 'Imagen de la diapositiva {$a}';
+$string['slideimage_desc'] = 'Sube una imagen para la diapositiva {$a} en el carrusel.';
+$string['carouselinterval'] = 'Intervalo del carrusel';
+$string['carouselintervaldesc'] = 'Introduce el tiempo de intervalo en milisegundos para las diapositivas del carrusel.';
 
 $string['carousel'] = 'Carrusel de imágenes';
 $string['loginsection'] = 'Sección de inicio de sesión';
 $string['previous'] = 'Ir a la diapositiva anterior';
 $string['next'] = 'Ir a la siguiente diapositiva';
 
-$string['slidetitle'] = 'Título para la diapositiva {$a}';
-$string['slidetitledesc'] = 'Ingrese el título para la diapositiva {$a}';
-$string['slideurl'] = 'URL del botón de la diapositiva {$a}';
-$string['slideurldesc'] = 'Ingrese la URL para el botón en la diapositiva {$a}. Esta URL se abrirá en una nueva ventana.';
-
-// Cadenas de autorización
-$string['unauthorized_access'] = 'Acceso No Autorizado';
+// Cadenas existentes para la autorización
+$string['unauthorized_access'] = 'Acceso no autorizado';
 $string['unauthorized_access_msg'] = 'Este sitio no está autorizado para operar en esta URL.';
 $string['devtools_access_disabled'] = 'El acceso a las herramientas de desarrollo está deshabilitado.';
 
-// Cadenas de visibilidad
+// Cadenas existentes para la visibilidad
 $string['hidefrontpagesections'] = 'Ocultar secciones superiores de la página principal';
-$string['hidefrontpagesections_desc'] = 'Habilite esta configuración para ocultar las secciones superiores de su página principal, proporcionando un aspecto más limpio.';
+$string['hidefrontpagesections_desc'] = 'Habilita esta opción para ocultar las secciones superiores de tu página principal, proporcionando una apariencia más limpia.';
 
 $string['hidefootersections'] = 'Ocultar secciones del pie de página';
-$string['hidefootersections_desc'] = 'Habilite esta configuración para ocultar secciones de su pie de página, proporcionando un aspecto más limpio.';
+$string['hidefootersections_desc'] = 'Habilita esta opción para ocultar secciones de tu pie de página, proporcionando una apariencia más limpia.';
 
-// Configuraciones de inicio de sesión y encabezado
-$string['themesettingslogin'] = 'Configuración básica de inicio de sesión';
-$string['personalareaheader'] = 'Imagen de Encabezado del Área Personal';
-$string['personalareaheaderdesc'] = 'Suba una imagen que se mostrará como encabezado en el área personal.';
-$string['mycoursesheader'] = 'Imagen de Encabezado de Mis Cursos';
-$string['mycoursesheaderdesc'] = 'Suba una imagen que se mostrará como encabezado en la página de mis cursos.';
+// Cadenas existentes para el inicio de sesión y encabezado
+$string['themesettingslogin'] = 'Ajustes básicos de inicio de sesión';
+$string['personalareaheader'] = 'Imagen de cabecera del área personal';
+$string['personalareaheaderdesc'] = 'Sube una imagen que se mostrará como cabecera en el área personal.';
+$string['mycoursesheader'] = 'Imagen de cabecera de Mis Cursos';
+$string['mycoursesheaderdesc'] = 'Sube una imagen que se mostrará como cabecera en la página de Mis Cursos.';
 
 // Definición de caché
 $string['cachedef_fontawesomeremuiiconmapping'] = 'Almacenamiento en caché para Font Awesome';
 
-// Nuevas cadenas necesarias para el settings.php actualizado
-$string['generalsettings'] = 'Configuración General';
-$string['generalnotice'] = 'Aviso General';
-$string['generalnoticedesc'] = 'Esto es útil para alertar a los usuarios de una situación particular. La alerta aparece en la portada.';
-$string['generalnotice_create'] = 'Configurar un aviso';
-$string['generalnoticeheading'] = 'Configuración de Avisos Generales';
-$string['generalnoticemode'] = 'Modo de Aviso General';
-$string['generalnoticemodedesc'] = 'Elija cómo mostrar los avisos generales';
+// Nuevas cadenas necesarias para el archivo settings.php actualizado
+$string['generalsettings'] = 'Ajustes generales';
+$string['generalnotice'] = 'Aviso general';
+$string['generalnoticedesc'] = 'Esto mostrará una alerta en tu página principal para avisar a los usuarios de un evento o situación particular.';
+$string['generalnotice_create'] = 'Crear un aviso';
+$string['generalnoticeheading'] = 'Ajustes del aviso general';
+$string['generalnoticemode'] = 'Modo de aviso general';
+$string['generalnoticemode_desc'] = 'Elige cómo mostrar los avisos generales';
 $string['generalnoticemode_off'] = 'Desactivado';
 $string['generalnoticemode_info'] = 'Información';
-$string['generalnoticemode_danger'] = 'Alerta Crítica';
+$string['generalnoticemode_danger'] = 'Alerta crítica';
 
-$string['chatheading'] = 'Configuración del Chat';
-$string['enable_chat'] = 'Habilitar Chat';
-$string['enable_chatdesc'] = 'Habilitar la funcionalidad de chat';
-$string['tawkto_embed_url'] = 'URL de Tawk.to';
-$string['tawkto_embed_urldesc'] = 'Ingrese su URL del widget de Tawk.to';
+$string['chatheading'] = 'Ajustes del chat';
+$string['enable_chat'] = 'Habilitar chat';
+$string['enable_chatdesc'] = 'Habilita la funcionalidad de chat';
+$string['tawkto_embed_url'] = 'URL de inserción de Tawk.to';
+$string['tawkto_embed_urldesc'] = 'Introduce la URL de tu widget de Tawk.to';
 
-$string['accessibilityheading'] = 'Configuración de Accesibilidad';
-$string['accessibility_widget'] = 'Widget de Accesibilidad';
-$string['accessibility_widgetdesc'] = 'Habilitar o deshabilitar el widget de accesibilidad';
+$string['accessibilityheading'] = 'Ajustes de accesibilidad';
+$string['accessibility_widget'] = 'Widget de accesibilidad';
+$string['accessibility_widgetdesc'] = 'Habilita o deshabilita el widget de accesibilidad';
 
-$string['contentprotectionheading'] = 'Configuración de Protección de Contenido';
-$string['copypaste_prevention'] = 'Prevenir Copiar/Pegar';
-$string['copypaste_preventiondesc'] = 'Habilitar la prevención de copiar y pegar para roles seleccionados';
-$string['copypaste_roles'] = 'Roles Protegidos';
-$string['copypaste_rolesdesc'] = 'Seleccione qué roles serán afectados por la prevención de copiar/pegar';
+$string['contentprotectionheading'] = 'Ajustes de protección de contenido';
+$string['copypaste_prevention'] = 'Prevenir copiar y pegar';
+$string['copypaste_preventiondesc'] = 'Habilita la prevención de copiar y pegar para roles seleccionados';
+$string['copypaste_roles'] = 'Roles protegidos';
+$string['copypaste_rolesdesc'] = 'Selecciona los roles que se verán afectados por la prevención de copiar y pegar';
 
-$string['loginsliderheading'] = 'Configuración del Slider de Inicio de Sesión';
+$string['loginsliderheading'] = 'Ajustes del deslizador de inicio de sesión';
 $string['slideheading'] = 'Diapositiva {$a}';
 
-$string['dashboardsettings'] = 'Configuración del Panel de Control';
-$string['personalareaheading'] = 'Configuración del Área Personal';
-$string['mycoursesheading'] = 'Configuración de Mis Cursos';
-$string['show_personalareaheader'] = 'Mostrar Encabezado del Área Personal';
-$string['show_personalareaheaderdesc'] = 'Mostrar u ocultar la imagen de encabezado del área personal';
-$string['show_mycoursesheader'] = 'Mostrar Encabezado de Mis Cursos';
-$string['show_mycoursesheaderdesc'] = 'Mostrar u ocultar la imagen de encabezado de mis cursos';
+$string['dashboardsettings'] = 'Ajustes del panel';
+$string['personalareaheading'] = 'Ajustes del área personal';
+$string['mycoursesheading'] = 'Ajustes de Mis Cursos';
+$string['show_personalareaheader'] = 'Mostrar cabecera del área personal';
+$string['show_personalareaheaderdesc'] = 'Mostrar u ocultar la imagen de cabecera del área personal';
+$string['show_mycoursesheader'] = 'Mostrar cabecera de Mis Cursos';
+$string['show_mycoursesheaderdesc'] = 'Mostrar u ocultar la imagen de cabecera de Mis Cursos';
 
-$string['footersettings'] = 'Configuración del Pie de Página';
-$string['footeraboutheading'] = 'Sección Acerca de del Pie de Página';
+$string['footersettings'] = 'Ajustes del pie de página';
+$string['footeraboutheading'] = 'Sección Acerca del pie de página';
 
-$string['loginsettings'] = 'Configuración de Inicio de Sesión';
+$string['loginsettings'] = 'Ajustes de inicio de sesión';
 
-// Mantener el texto predeterminado de "acerca de"
+// Mantener el texto "Acerca de" por defecto
 $string['abouttext_default'] = '<p style="text-align: center;"></p>
-    <p style="text-align: left;">
-    </p>
-    <p style="text-align: center;"><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><b>IngeWeb - Soluciones para triunfar en Internet<br></b></span><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">Expertos en Moodle, BigBlueButton, WordPress y Joomla.<br></span><strong style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">www.ingeweb.co</strong></p>
-    <p></p>';
+<p style="text-align: left;">
+</p>
+<p style="text-align: center;"><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><b>OrionCloud - Hosting que impulsa tu éxito</b></span><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><br></span><strong style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">www.orioncloud.com.co</strong></p>
+<p></p>';
 
-$string['clickpointhelp'] = "Ayuda de la Plataforma";
-$string['clickpointfeedback'] = "Feedback de clickpoint";
-$string['clickpointsupport'] = "Soporte clickpoint";
+$string['clickpointhelp'] = "Ayuda de la plataforma";
+$string['clickpointfeedback'] = "Retroalimentación de ClickPoint";
+$string['clickpointsupport'] = "Soporte de ClickPoint";
 
-$string['enrolleduserscountvisibilityhead'] = "Mostrar información de 'Estudiantes matriculados'";
-$string['enrolleduserscountvisibilitydesc'] = "Deshabilitar para ocultar la información de 'Estudiantes matriculados'";
+$string['enrolleduserscountvisibilityhead'] = "Mostrar información de 'Estudiantes inscritos'";
+$string['enrolleduserscountvisibilitydesc'] = "Desactiva para ocultar la información de 'Estudiantes inscritos'";
 
-$string['police_autorization']='Autorización para el tratamiento de datos en Medios de Recolección';
-$string['police_autorization_help']='Al diligenciar este formulario autorizo a CAJA SANTANDEREANA DE SUBSIDIO FAMILIAR "clickpoint", ubicada en CARRERA 27 # 61 – 78 PUERTA DEL SOL y con teléfono de contacto 6434444, para que recolecte, almacene, use, circule y/o suprima mis datos personales con la finalidad de realizar el registro para realización de cursos virtuales y las demás  finalidades incorporadas en la Política de Tratamiento de Información disponible en www.clickpoint.com, la cual declaro conocer. Así mismo, conozco que como titular me asisten los derechos a conocer, actualizar, rectificar y suprimir mis datos y revocar la autorización.';
+// Agregar la cadena faltante
+$string['defaultheader'] = 'Cabecera por defecto';
